7663e88ce1d4995d1285f62626abbf042b9ac013
WaelKendouli <kendouliwael@gmail.com>
2026-07-31 19:12:37 +0200

    Adding calculating total price logic and reset form

--- PizzaLogic.js
+++ PizzaLogic.js
@@ -18,8 +18,10 @@ const ckSausage = document.getElementById("ckSausage");
 const ckBacon = document.getElementById("ckBacon");
 const ckCheese = document.getElementById("ckCheese");
 
-let Toppings = "";
+const btnReset = document.getElementById("btnReset");
 
+let Toppings = "";
+let Total = 0;
 function AddToppings(element)
 {
     if (!element.hasAttribute("data-topping")) {
@@ -59,12 +61,52 @@ else
 }
 }
 
+function UpdateTotalPrice(element)
+{
+ if (!element.hasAttribute("data-price") ) {
+        return ;
+    }
+    Total = 0;
 
+    let AllRadios = document.querySelectorAll('input[type="radio"][data-price]');
+    AllRadios.forEach(radio => {
+        if (radio.checked) {
+             Total += Number(radio.dataset.price);
+        }
+    });
+      let AllCheckBox = document.querySelectorAll('input[type="checkbox"][data-price]');
+    AllCheckBox.forEach(radio => {
+        if (radio.checked) {
+             Total += Number(radio.dataset.price);
+        }
+    });
+        UpdateLabelUI(element , "data-price" , Total + " $" , lbTotal);
+}
 
+function ResetForm()
+{
+    document.location.reload();
+}
+btnReset.addEventListener("click", ResetForm);
 
-rbSmall.addEventListener("change",function() { UpdateTotalPriceForSelectingSize(this); });
-rbMed.addEventListener("change",function() { UpdateTotalPriceForSelectingSize(this); });
-rbLarge.addEventListener("change",function() { UpdateTotalPriceForSelectingSize(this); });
+rbSmall.addEventListener("change",function() { UpdateTotalPrice(this); });
+rbMed.addEventListener("change",function() { UpdateTotalPrice(this); });
+rbLarge.addEventListener("change",function() { UpdateTotalPrice(this); });
+rbThick.addEventListener("change",function() { UpdateTotalPrice(this); });
+rbThin.addEventListener("change",function() { UpdateTotalPrice(this); });
+rbEatin.addEventListener("change",function() { UpdateTotalPrice(this); });
+rbEatOut.addEventListener("change",function() { UpdateTotalPrice(this); });
+ckPepperoni.addEventListener("change",function() { UpdateTotalPrice(this); });
+ckMushrooms.addEventListener("change",function() { UpdateTotalPrice(this); });
+ckOnions.addEventListener("change",function() { UpdateTotalPrice(this); });
+ckSausage.addEventListener("change",function() { UpdateTotalPrice(this); });
+ckBacon.addEventListener("change",function() { UpdateTotalPrice(this); });
+ckCheese.addEventListener("change",function() { UpdateTotalPrice(this); });
+
+rbSmall.addEventListener("change",function() { UpdateLabelUI(this , "data-size" , this.dataset.size , lbSize); });
+rbMed.addEventListener("change",function() { UpdateLabelUI(this , "data-size" , this.dataset.size , lbSize); });
+rbLarge.addEventListener("change",function() { UpdateLabelUI(this , "data-size" , this.dataset.size , lbSize); });
+
 rbThick.addEventListener("change",
     function() { UpdateLabelUI(this , "data-Thicknes" , this.dataset.thicknes , lbCrust); });
 rbThin.addEventListener("change",
